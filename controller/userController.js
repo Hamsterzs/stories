92783bcd3791cbcde998ce4cf325b8093c3f91c2
abel <abel090713@gmail.com>
2021-01-17 20:57:56 -0600

    Auth finished

--- controller/userController.js
+++ controller/userController.js
@@ -1,7 +1,7 @@
 const User = require("../model/User")
 const bcrypt = require("bcrypt")
 
-exports.createUser = async (req, res) => {
+exports.createUser = async (req, res, next) => {
     try {
         const existingUser = await User.findOne({ username: req.body.username })
         if (existingUser) {
@@ -10,20 +10,8 @@ exports.createUser = async (req, res) => {
         }
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
         const user = await User.create({ username: req.body.username, password: hashedPassword })
-        res.status(200).json(req.user)
+        next()
     } catch (error) {
         console.log(error);
-    }
-}
-
-exports.verifyUser = (req, res) => {
-    try {
-        if (req.user) {
-            res.status(200).json({ message: "Signed in succesfull", user: req.user })
-        } else {
-            res.status(200).json({ message: "Signed in failed" })
-        }
-    } catch (error) {
-        console.log(res.status(500).json({ message: error }));
     }
 }
--- routes/router.js
+++ routes/router.js
@@ -2,15 +2,18 @@ const router = require("express").Router()
 const UserController = require("../controller/userController")
 const passport = require("passport")
 
-router.post("/sign-up",
-    UserController.createUser
-)
-
-router.post("/sign-in", (req, res, next) => {
+const authenticate = (req, res) => {
     passport.authenticate("local", (err, user, info) => {
         if (user) res.status(info.status).json(user)
         else res.status(info.status).json(info.message)
-    })(req, res, next)
-})
+    })(req, res)
+}
+
+router.post("/sign-up",
+    UserController.createUser,
+    authenticate
+)
+
+router.post("/sign-in", authenticate)
 
 module.exports = router
