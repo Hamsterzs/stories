5f48f11c97e78caa6c236b193970818a482363c6
abel <abel090713@gmail.com>
2021-01-27 19:20:27 -0600

    failed login messages

--- routes/router.js
+++ routes/router.js
@@ -9,9 +9,16 @@ router.post("/sign-up",
     UserController.getUser
 )
 
-router.post("/sign-in",
-    passport.authenticate("local"),
-    UserController.getUser
+router.post("/sign-in", (req, res) => {
+    passport.authenticate("local", (err, user, info) => {
+        if (user) {
+            req.logIn(user, err => {
+                if (err) console.log(err);
+            })
+        }
+        res.status(info.status).json({ user: user, message: info.message })
+    })(req, res)
+}
 )
 
 router.post("/sign-out", UserController.signOut)
